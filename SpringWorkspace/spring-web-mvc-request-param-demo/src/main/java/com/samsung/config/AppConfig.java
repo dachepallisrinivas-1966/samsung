623bb66f777112536d6ad79b97932b5c8db4d754
Dachepalli Srinivas <dachepallisrinivas@gmail.com>
2022-02-16 15:20:58 +0530

    16-feb-2022

--- SpringWorkspace/spring-web-mvc-request-param-demo/src/main/java/com/samsung/config/AppConfig.java
+++ SpringWorkspace/spring-web-mvc-request-param-demo/src/main/java/com/samsung/config/AppConfig.java
@@ -15,13 +15,13 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan(basePackages = { "com.samsung" })
 public class AppConfig {
 
-	@Bean
-	public InternalResourceViewResolver resolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setViewClass(JstlView.class);
-		resolver.setPrefix("/WEB-INF/view/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
+//	@Bean
+//	public InternalResourceViewResolver resolver() {
+//		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//		//resolver.setViewClass(JstlView.class);
+//		resolver.setPrefix("/WEB-INF/view/");
+//		resolver.setSuffix(".jsp");
+//		return resolver;
+//	}
 
 }
